3ee6fc31cb13991676bb57f3d06db9ab93074a5b
VIctor <colen.victor@icloud.com>
2024-08-14 05:52:34 -0300

    minor changes

--- Implementacao 2/Implementação/plot_operations.py
+++ Implementacao 2/Implementação/plot_operations.py
@@ -1,14 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Carregar os dados do CSV
 data = pd.read_csv('operations_count.csv')
-
-# Plotar os dados
 plt.figure(figsize=(10, 6))
 plt.bar(data['Algorithm'], data['Operations'], color='skyblue')
 plt.xlabel('Algorithm')
 plt.ylabel('Number of Operations')
 plt.title('Number of Operations for Each Search Algorithm')
-plt.savefig('operations_plot.png')  # Salva o gráfico como imagem
-plt.show()  # Exibe o gráfico
+plt.savefig('operations_plot.png') 
+plt.show() 
