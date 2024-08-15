a478541c06c94905eb3136442f00fa62abb8ce25
VIctor <colen.victor@icloud.com>
2024-08-15 11:12:23 -0300

    minor adjustments

--- Implementacao 2/Implementação/main.cpp
+++ Implementacao 2/Implementação/main.cpp
@@ -5,71 +5,6 @@
 
 using namespace std;
 
-class SearchAlgorithms
-{
-
-public:
-    static bool SimpleLinearSearch(int *array, int sizeOfArray, int x, int &operationsCount)
-    {
-        operationsCount += 2; // int i; i = 0;
-        for (int i = 0; i < sizeOfArray; i++)
-        {
-            operationsCount++; // i < sizeOfArray
-            if (x == array[i])
-            {
-                operationsCount++; // return
-                return true;
-            }
-            operationsCount++; // if (x == array[i])
-        }
-        operationsCount += 2; // i < sizeOfArray; return false
-        return false;
-    }
-
-    static bool BinarySearch(int *array, int left, int right, int x, int &operationsCount)
-    {
-        while (left <= right)
-        {
-            operationsCount++; // while (left <= right)
-            int mid = left + (right - left) / 2;
-            operationsCount += 4; // int mid = left + (right - left) / 2;
-            operationsCount++;    //  if (array[mid] == x)
-            if (array[mid] == x)
-            {
-                operationsCount++; // return true
-                return true;
-            }
-            operationsCount++; // if (array[mid] < x)
-            if (array[mid] < x)
-            {
-                operationsCount += 2; // left = mid + 1
-                left = mid + 1;
-            }
-            else
-            {
-                operationsCount += 2; // right = mid - 1;
-                right = mid - 1;
-            }
-        }
-        operationsCount += 2; // while (left <= right); return false;
-        return false;
-    }
-
-    static bool SentinelLinearSearch(int *array, int lastIndex, int x, int &operationsCount)
-    {
-        int i = 0;
-        operationsCount += 2; // int i = 0
-        array[lastIndex] = x; 
-        operationsCount += 2; // array[lastIndex] = x;
-        while (array[i] != x) {
-            i++;
-            operationsCount += 2; //  while (array[i] != x); i++;
-        }
-        operationsCount += 3; // while (array[i] != x); return i != lastIndex
-        return i != lastIndex;
-    }
-};
-
 void TestSimpleLinearSearch(int *megaArray, std::map<std::string, int> &operationsCountByAlgorithm);
 void TestBinarySearch(int *megaArray, std::map<std::string, int> &operationsCountByAlgorithm);
 void TestSentinelLinearSearch(int *megaArray, std::map<std::string, int> &operationsCountByAlgorithm);
@@ -185,20 +120,14 @@ void populateMegaArray(int *megaArray)
 void ExportToCsv(const std::map<std::string, int>& operationsCountByAlgorithm)
 {
     std::ofstream file("operations_count.csv");
-
     if (!file.is_open()) {
         std::cerr << "Failed to open the file for writing." << std::endl;
         return;
     }
-
-    // Write the CSV header
     file << "Algorithm,Operations\n";
-
-    // Iterate over the map and write each entry to the CSV
     for (const auto& entry : operationsCountByAlgorithm) {
         file << entry.first << "," << entry.second << "\n";
     }
-
     file.close();
     std::cout << "Data successfully exported to operations_count.csv" << std::endl;
 }
@@ -208,3 +137,68 @@ void PlotCharts()
     system("python3 plot_operations.py"); // On Linux or MacOS
     // system("python plot_operations.py");  // On Windows
 }
+
+class SearchAlgorithms
+{
+
+public:
+    static bool SimpleLinearSearch(int *array, int sizeOfArray, int x, int &operationsCount)
+    {
+        operationsCount += 2; // int i; i = 0;
+        for (int i = 0; i < sizeOfArray; i++)
+        {
+            operationsCount++; // i < sizeOfArray
+            if (x == array[i])
+            {
+                operationsCount++; // return
+                return true;
+            }
+            operationsCount++; // if (x == array[i])
+        }
+        operationsCount += 2; // i < sizeOfArray; return false
+        return false;
+    }
+
+    static bool BinarySearch(int *array, int left, int right, int x, int &operationsCount)
+    {
+        while (left <= right)
+        {
+            operationsCount++; // while (left <= right)
+            int mid = left + (right - left) / 2;
+            operationsCount += 4; // int mid = left + (right - left) / 2;
+            operationsCount++;    //  if (array[mid] == x)
+            if (array[mid] == x)
+            {
+                operationsCount++; // return true
+                return true;
+            }
+            operationsCount++; // if (array[mid] < x)
+            if (array[mid] < x)
+            {
+                operationsCount += 2; // left = mid + 1
+                left = mid + 1;
+            }
+            else
+            {
+                operationsCount += 2; // right = mid - 1;
+                right = mid - 1;
+            }
+        }
+        operationsCount += 2; // while (left <= right); return false;
+        return false;
+    }
+
+    static bool SentinelLinearSearch(int *array, int lastIndex, int x, int &operationsCount)
+    {
+        int i = 0;
+        operationsCount += 2; // int i = 0
+        array[lastIndex] = x; 
+        operationsCount += 2; // array[lastIndex] = x;
+        while (array[i] != x) {
+            i++;
+            operationsCount += 2; //  while (array[i] != x); i++;
+        }
+        operationsCount += 3; // while (array[i] != x); return i != lastIndex
+        return i != lastIndex;
+    }
+};
