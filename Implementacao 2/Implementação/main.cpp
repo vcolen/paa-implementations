132e5763f0e0af9eba552880b676cbab3ce657b2
VIctor <colen.victor@icloud.com>
2024-08-15 10:52:53 -0300

    count operations for all algorithms

--- Implementacao 2/Implementação/main.cpp
+++ Implementacao 2/Implementação/main.cpp
@@ -9,59 +9,81 @@ class SearchAlgorithms
 {
 
 public:
-    static bool SimpleLinearSearch(int *array, int sizeOfArray, int x, std::map<std::string, int> &dict)
+    static bool SimpleLinearSearch(int *array, int sizeOfArray, int x, int &operationsCount)
     {
-        dict["SimpleLinearSearch"] += 2; // int i; i = 0;
+        operationsCount += 2; // int i; i = 0;
         for (int i = 0; i < sizeOfArray; i++)
         {
+            operationsCount++; // i < sizeOfArray
             if (x == array[i])
             {
-                dict["SimpleLinearSearch"]++; // return
+                operationsCount++; // return
                 return true;
             }
-            dict["SimpleLinearSearch"]+=2; // x == array[i]; i < sizeOfArray
+            operationsCount++; // if (x == array[i])
         }
-        dict["SimpleLinearSearch"] += 2; // i < sizeOfArray; return false
-        return false; 
+        operationsCount += 2; // i < sizeOfArray; return false
+        return false;
     }
 
-    static bool SentinelLinearSearch(int *array, int lastIndex, int x, std::map<std::string, int> &dict)
-    {
-        int i = 0;
-        array[lastIndex] = x;
-        while (array[i] != x)
-            i++;
-        return i != lastIndex;
-    }
-
-    static bool BinarySearch(int *array, int left, int right, int x, std::map<std::string, int> &dict)
+    static bool BinarySearch(int *array, int left, int right, int x, int &operationsCount)
     {
         while (left <= right)
         {
+            operationsCount++; // while (left <= right)
             int mid = left + (right - left) / 2;
+            operationsCount += 4; // int mid = left + (right - left) / 2;
+            operationsCount++;    //  if (array[mid] == x)
             if (array[mid] == x)
+            {
+                operationsCount++; // return true
                 return true;
+            }
+            operationsCount++; // if (array[mid] < x)
             if (array[mid] < x)
+            {
+                operationsCount += 2; // left = mid + 1
                 left = mid + 1;
+            }
             else
+            {
+                operationsCount += 2; // right = mid - 1;
                 right = mid - 1;
+            }
         }
+        operationsCount += 2; // while (left <= right); return false;
         return false;
+    }
+
+    static bool SentinelLinearSearch(int *array, int lastIndex, int x, int &operationsCount)
+    {
+        int i = 0;
+        operationsCount += 2; // int i = 0
+        array[lastIndex] = x; 
+        operationsCount += 2; // array[lastIndex] = x;
+        while (array[i] != x) {
+            i++;
+            operationsCount += 2; //  while (array[i] != x); i++;
+        }
+        operationsCount += 3; // while (array[i] != x); return i != lastIndex
+        return i != lastIndex;
     }
 };
 
-void populateMegaArray(int *megaArray) {
-    for(int i = 0; i < 1000000; i++) {
+void populateMegaArray(int *megaArray)
+{
+    for (int i = 0; i < 1000000; i++)
+    {
         megaArray[i] = i;
     }
 }
-void ExportToCsv(int operationCount)
+void ExportToCsv(int operationsCount)
 {
     std::ofstream file("operations_count.csv");
     file << "Algorithm,Operations\n";
-    file << "SimpleLinearSearch," << operationCount << "\n";
-    file << "BinarySearch," << operationCount << "\n";
-    file << "SentinelLinearSearch," << operationCount << "\n";
+    file << "SimpleLinearSearch," << operationsCount << "\n";
+    file << "BinarySearch," << operationsCount << "\n";
+    file << "SentinelLinearSearch," << operationsCount << "\n";
     file.close();
 }
 
@@ -73,17 +95,23 @@ void PlotCharts()
 
 int main()
 {
+    map<string, int> operationsCountByAlgorithm;
     int megaArray[1000000];
     populateMegaArray(megaArray);
-    std::map<std::string, int> operationCountByAlgorithm;
-    operationCountByAlgorithm["SimpleLinearSearch"] = 0;
-    SearchAlgorithms::SimpleLinearSearch(megaArray, 1000000, 1000001, operationCountByAlgorithm);
-    cout << "SimpleLinearSearch - Operations: " << operationCountByAlgorithm["SimpleLinearSearch"] << endl;
-    //operationCountByAlgorithm["BinarySearch"] = 0;
-    // int result2 = SearchAlgorithms::BinarySearch(test, 0, 10, 11, operationCount);
-    // cout << "BinarySearch - Result: " << result2 << ", Operations: " << operationCount << endl;
-    // operationCountByAlgorithm["SentinelLinearSearch"] = 0;
-    // int result3 = SearchAlgorithms::SentinelLinearSearch(test, 10, 11, operationCount);
-    // cout << "SentinelLinearSearch - Result: " << result3 << ", Operations: " << operationCount << endl;
+
+    int operationsCount = 0;
+    SearchAlgorithms::SimpleLinearSearch(megaArray, 1000000, 1000001, operationsCount);
+    operationsCountByAlgorithm["SimpleLinearSearch"] = operationsCount;
+    cout << "SimpleLinearSearch - Operations: " << operationsCountByAlgorithm["SimpleLinearSearch"] << endl;
+
+    operationsCount = 0;
+    SearchAlgorithms::BinarySearch(megaArray, 0, 1000000, 1000001, operationsCount);
+    operationsCountByAlgorithm["BinarySearch"] = operationsCount;
+    cout << "BinarySearch - Operations: " << operationsCountByAlgorithm["BinarySearch"] << endl;
+
+    operationsCount = 0;
+    SearchAlgorithms::SentinelLinearSearch(megaArray, 999999, 1000000, operationsCount);
+    operationsCountByAlgorithm["SentinelLinearSearch"] = operationsCount;
+    cout << "SentinelLinearSearch - Operations: " << operationsCount << endl;
     return 0;
 }
